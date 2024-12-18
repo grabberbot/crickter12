2bf95922d687105c12e25edeb6964c2c6ebafca6
Shivam <146311656+Shivam1232241@users.noreply.github.com>
2024-12-18 17:39:18 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,13 +3,13 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6977793872"
-    sudo_users = "6845325416", "6765826972"
+    sudo_users = "6977793872", "6977793872"
     GROUP_ID = -1002133191051
     TOKEN = "7714215608:AAHGp9x86-wvlTityW8k6vfa1sLE_-6PIhI"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
+    SUPPORT_CHAT = "CricketGrabberBot"
+    UPDATE_CHAT = "CricketGrabberBot"
     BOT_USERNAME = "CricketGrabberBot"
     CHARA_CHANNEL_ID = "-1002133191051"
     api_id = 28735016
