6222bf3263c5d1e765866d342f1cb93e8a6b9af4
Shivam <146311656+Shivam1232241@users.noreply.github.com>
2024-12-18 20:52:41 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "7053677913", "6977793872"
     GROUP_ID = -1002143253592
     TOKEN = "7714215608:AAFB0VovsvLpMmMXIvE6NGUbCABJnAFQYk4"
-    mongo_url = "mongodb+srv://kalawativerma80:s?QF7Yx3NUE9u!6@finepixel.afi4pra.mongodb.net/?retryWrites=true&w=majority&appName=finepixel"
+    mongo_url = "mongodb+srv://kalawativerma80:haremdbforbot@finepixel.afi4pra.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "The2B2T"
     UPDATE_CHAT = "The2B2T"
