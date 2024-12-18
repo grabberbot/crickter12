8353d773049a5d5856f6fc07a8747f14ec2e59df
Shivam <146311656+Shivam1232241@users.noreply.github.com>
2024-12-18 21:44:23 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "7053677913", "6977793872"
     GROUP_ID = -1002143253592
     TOKEN = "7308335879:AAHTi3IjB6OcOtxsutJfAEdnfCuMnis5X6Y"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://kalawativerma80:haremdbforbot@finepixel.afi4pra.mongodb.net/?retryWrites=true&w=majority&appName=finepixel"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "The2B2T"
     UPDATE_CHAT = "The2B2T"
