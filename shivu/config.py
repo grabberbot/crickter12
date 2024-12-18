0e16923867cdae703ac509ec4b1d8b70d7cebbc9
Shivam <146311656+Shivam1232241@users.noreply.github.com>
2024-12-18 18:40:33 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "6977793872"
     sudo_users = "6977793872", "6977793872"
     GROUP_ID = -1002143253592
-    TOKEN = "7714215608:AAHGp9x86-wvlTityW8k6vfa1sLE_-6PIhI"
+    TOKEN = "7714215608:AAFB0VovsvLpMmMXIvE6NGUbCABJnAFQYk4"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "The2B2T"
