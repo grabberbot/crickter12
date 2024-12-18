344ded73961dff2614a067f0640db4a5ba56227a
Shivam <146311656+Shivam1232241@users.noreply.github.com>
2024-12-18 20:59:58 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,8 +5,8 @@ class Config(object):
     OWNER_ID = "6977793872"
     sudo_users = "7053677913", "6977793872"
     GROUP_ID = -1002143253592
-    TOKEN = "7714215608:AAFB0VovsvLpMmMXIvE6NGUbCABJnAFQYk4"
-    mongo_url = "mongodb+srv://kalawativerma80:haremdbforbot@finepixel.afi4pra.mongodb.net/?retryWrites=true&w=majority"
+    TOKEN = "7308335879:AAHTi3IjB6OcOtxsutJfAEdnfCuMnis5X6Y"
+    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "The2B2T"
     UPDATE_CHAT = "The2B2T"
