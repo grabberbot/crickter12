636dd6ea2c5145a0cd84438df3f71da463482ab6
Shivam <146311656+Shivam1232241@users.noreply.github.com>
2024-12-19 17:49:50 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,15 +3,15 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6977793872"
-    sudo_users = "7053677913", "1732582235", "6977793872"
-    GROUP_ID = -1002437903458,
-    TOKEN = "7308335879:AAHTi3IjB6OcOtxsutJfAEdnfCuMnis5X6Y"
+    sudo_users = "6977793872"
+    GROUP_ID = -1002342573806
+    TOKEN = "7714215608:AAG3f6YdhsM3NTzskUhoPDy3xu4LFCw7EuE"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "GetCricketPlayers"
-    UPDATE_CHAT = "GetCricketPlayers"
-    BOT_USERNAME = "Get_Cricket_Players_Bot"
-    CHARA_CHANNEL_ID = "-1002437903458"
+    SUPPORT_CHAT = "+YMBvMgB5qTA5NjY9"
+    UPDATE_CHAT = "+YMBvMgB5qTA5NjY9"
+    BOT_USERNAME = "CricketGrabberBot"
+    CHARA_CHANNEL_ID = "-1002342573806"
     api_id = 28735016
     api_hash = "395761ed41e18de91ee4e18ff99afc81"
 
