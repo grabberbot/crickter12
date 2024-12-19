736ee8c14e9aba1b41dc9af50061fdc60dcdfd45
Shivam <146311656+Shivam1232241@users.noreply.github.com>
2024-12-19 07:46:28 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "6977793872"
     sudo_users = "7053677913", "6977793872"
     GROUP_ID = -1002143253592
-    TOKEN = "7308335879:AAHTi3IjB6OcOtxsutJfAEdnfCuMnis5X6Y"
+    TOKEN = "7714215608:AAG3f6YdhsM3NTzskUhoPDy3xu4LFCw7EuE"
     mongo_url = "mongodb+srv://kalawativerma80:haremdbforbot@finepixel.afi4pra.mongodb.net/?retryWrites=true&w=majority&appName=finepixel"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "The2B2T"
